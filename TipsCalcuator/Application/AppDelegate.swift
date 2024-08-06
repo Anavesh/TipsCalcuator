ab016c78f3c41e455adee652c1936a4a38396b42
Anavesh <naravan9x@gmail.com>
2024-08-06 10:44:19 -0300

    Added only support of portrait orientation

--- TipsCalcuator/Application/AppDelegate.swift
+++ TipsCalcuator/Application/AppDelegate.swift
@@ -14,5 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
 
